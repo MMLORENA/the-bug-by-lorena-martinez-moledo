509187108dc132bffe290bba5dbb9782810e6eb2
Lorena Martinez <lmartmol@gmail.com>
2023-06-01 13:13:32 +0200

    Refactor writeLogToFile test whithout mock & test readLogFromFile

--- src/server/logs/LogManager/LogManager.test.ts
+++ src/server/logs/LogManager/LogManager.test.ts
@@ -1,9 +1,9 @@
 import fsSync from "fs";
-import fs from "fs/promises";
 import path from "path";
 import LogManager from "./LogManager";
 
 describe("Given an instance of LogManager", () => {
+  const fakeLog = "log";
   const fileName = "fakeFileName";
   const folderName = "fakeFolderName";
   const fakePath = path.join(folderName, fileName);
@@ -13,16 +13,30 @@ describe("Given an instance of LogManager", () => {
     fsSync.rmSync(folderName, { recursive: true, force: true });
   });
 
-  describe("When the method writeLogFile it's invoked with 'log'", () => {
-    test("Then the method appendFile of fs should be invoked with 'fakeFolderName\\fakeFileName' as path and the log received", async () => {
-      const spyAppendFile = jest.spyOn(fs, "appendFile");
+  describe("When the method writeLogToFile it's invoked with 'log'", () => {
+    test("Then it should be 'log' write into 'fakeFolderName\\fakeFileName' path", async () => {
+      const logManager = new LogManager(fileName, folderName);
 
-      const log = "log";
-      const writeLuisLogToFile = new LogManager(fileName, folderName);
+      await logManager.writeLogToFile(fakeLog);
+      const resultLogData = await logManager.readLogToFile(fakePath);
 
-      await writeLuisLogToFile.writeLogToFile(log);
+      expect(resultLogData).toBe(fakeLog);
+    });
+  });
 
-      expect(spyAppendFile).toHaveBeenCalledWith(fakePath, log);
+  describe("When the method readLogFromFile it's invoked with 'fakeFolderName\\fakeFileName'", () => {
+    beforeAll(async () => {
+      const logManagerFake = new LogManager(fileName, folderName);
+
+      await logManagerFake.writeLogToFile(fakeLog);
+    });
+
+    test("Then the method readLogFromFile should return the log inside the given path with 'log'", async () => {
+      const logManager = new LogManager(fileName, folderName);
+
+      const resultLogData = await logManager.readLogToFile(fakePath);
+
+      expect(resultLogData).toBe(fakeLog);
     });
   });
 });
