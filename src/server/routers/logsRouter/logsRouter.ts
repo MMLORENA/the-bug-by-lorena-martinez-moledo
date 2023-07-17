72294f06fc0684f7276055c0724833def2468691
Robert Belenguer Cotaina <rob.belenguer@gmail.com>
2023-07-17 17:30:35 +0200

    Add get-logs-files endpoint on logsRouter

--- src/server/routers/logsRouter/logsRouter.ts
+++ src/server/routers/logsRouter/logsRouter.ts
@@ -1,6 +1,18 @@
 import { Router } from "express";
+import { partialPaths } from "../paths";
+import getLogsFilesController from "../../controllers/logsControllers/getLogsFilesController/getLogsFilesController";
+import LogManager from "../../logs/LogManager/LogManager";
+import auth from "../../middlewares/auth/auth";
 
 // eslint-disable-next-line new-cap
 const logsRouter = Router();
+
+const logManager = new LogManager("sessions");
+
+logsRouter.get(
+  partialPaths.logs.getLogsFiles,
+  auth,
+  getLogsFilesController(logManager)
+);
 
 export default logsRouter;
