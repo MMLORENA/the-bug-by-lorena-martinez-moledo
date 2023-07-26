d4072bc7018173a05984d3b8974fc79632f2815e
Lorena Martinez <lmartmol@gmail.com>
2023-07-26 19:08:10 +0200

    Add logByDateSchema to validate req params

---
+++ src/server/schemas/logByDateSchema.ts
@@ -0,0 +1,18 @@
+import { Joi } from "express-validation";
+import joiTypesError from "./joiTypesErrors.js";
+
+const { stringEmpty, stringPatternName } = joiTypesError;
+
+const logByDateSchema = {
+  params: Joi.object({
+    date: Joi.string()
+      .pattern(/^\d{2}-\d{2}-\d{4}$/, "DD-MM-YYYY")
+      .required()
+      .messages({
+        [stringPatternName]: "Invalid date format. Format must be DD-MM-YYYY",
+        [stringEmpty]: "Date params is required",
+      }),
+  }),
+};
+
+export default logByDateSchema;
