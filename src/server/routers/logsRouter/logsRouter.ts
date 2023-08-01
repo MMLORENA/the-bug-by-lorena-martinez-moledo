5480fb87c9616fd9346cbb5195135b4da408be8f
Lorena Martinez <lmartmol@gmail.com>
2023-08-01 13:35:51 +0200

    Add getLogByDate endpoint in logs router

--- src/server/routers/logsRouter/logsRouter.ts
+++ src/server/routers/logsRouter/logsRouter.ts
@@ -1,10 +1,16 @@
 import cookieParser from "cookie-parser";
 import { Router } from "express";
-import { getLogsFilesController } from "../../controllers/getLogsControllers/getLogsControllers.js";
+import { validate } from "express-validation";
+import { environment } from "../../../environment/loadEnvironments.js";
+import {
+  getLogByDateController,
+  getLogsFilesController,
+} from "../../controllers/getLogsControllers/getLogsControllers.js";
 import LogManager from "../../logs/LogManager/LogManager.js";
 import { auth, checkIsUserAdmin } from "../../middlewares/auth/auth.js";
-import { environment } from "../../../environment/loadEnvironments.js";
-import { paths } from "../paths.js";
+import getLogByDateSchema from "../../schemas/getLogByDateSchema.js";
+import { noAbortEarly } from "../../schemas/validateOptions.js";
+import { partialPaths, paths } from "../paths.js";
 
 const { logsRootFolder } = environment;
 
@@ -18,6 +24,14 @@ logsRouter.get(
   auth,
   checkIsUserAdmin,
   getLogsFilesController(logManager)
+);
+
+logsRouter.get(
+  partialPaths.logs.logByDate,
+  validate(getLogByDateSchema, {}, noAbortEarly),
+  auth,
+  checkIsUserAdmin,
+  getLogByDateController(logManager)
 );
 
 export default logsRouter;
