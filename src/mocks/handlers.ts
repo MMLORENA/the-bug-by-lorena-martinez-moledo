6ca2b68bb571757129bb008117ef26223aef21ec
Lorena Martinez <lmartmol@gmail.com>
2023-09-16 23:17:44 +0200

    Install msw

---
+++ src/mocks/handlers.ts
@@ -0,0 +1,11 @@
+import { rest } from "msw";
+import { apiPartialPaths } from "../constants/apiPaths/apiPaths";
+import { mocksBugs } from "../factories/bugsFactory";
+
+const apiUrl = import.meta.env.VITE_API_URL;
+
+export const handlers = [
+  rest.get(`${apiUrl}${apiPartialPaths.base}`, (_req, res, ctx) =>
+    res(ctx.status(200), ctx.json(mocksBugs))
+  ),
+];
