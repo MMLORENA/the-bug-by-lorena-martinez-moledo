72f5c6440e571e220e18c67007e0fdb8820ba0c2
Lorena Martinez <lmartmol@gmail.com>
2023-09-16 18:42:48 +0200

    Add BugCategory enum type

--- src/types.ts
+++ src/types.ts
@@ -1,8 +1,16 @@
+export enum BugCategory {
+  logicalBugs = "Logical Bugs",
+  syntaxErrors = "Syntax Errors",
+  uIuXProblems = "UI/UX Problems",
+  performanceIssues = "Performance Issues",
+  securityVulnerabilities = "Security Vulnerabilities",
+}
+
 export interface Bug {
   id: string;
   name: string;
   description: string;
-  category: string;
+  category: BugCategory;
   picture: string;
 }
 
