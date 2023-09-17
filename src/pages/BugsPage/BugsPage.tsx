4df6d2b68e7a685029a4f8010d310e6b131e768f
Lorena Martinez <lmartmol@gmail.com>
2023-09-17 10:03:57 +0200

    Invoke getBugs in BugsPage

--- src/pages/BugsPage/BugsPage.tsx
+++ src/pages/BugsPage/BugsPage.tsx
@@ -1,6 +1,21 @@
+import { useEffect } from "react";
+import useBugs from "../../hooks/useBugs/useBugs";
+import { useAppDispatch } from "../../store";
+import { loadBugsActionCreator } from "../../store/bugs/bugsSlice";
 import "./BugsPage.scss";
 
 const BugsPage = (): React.ReactElement => {
+  const { getBugs } = useBugs();
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    (async () => {
+      const bugs = await getBugs();
+
+      dispatch(loadBugsActionCreator(bugs));
+    })();
+  }, [getBugs, dispatch]);
+
   return <h1 className="bugs-page-title">Debug Directory</h1>;
 };
 
