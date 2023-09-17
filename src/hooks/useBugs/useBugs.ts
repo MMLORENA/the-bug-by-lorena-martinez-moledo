90c80f85f559fb95f634c23146b22606fa3849fe
Lorena Martinez <lmartmol@gmail.com>
2023-09-17 22:43:46 +0200

    Add dispath showLoading & hideLoading action creators in getBugs

--- src/hooks/useBugs/useBugs.ts
+++ src/hooks/useBugs/useBugs.ts
@@ -2,15 +2,26 @@ import axios from "axios";
 import { useCallback } from "react";
 import { Bugs } from "../../types";
 import { apiPartialPaths, apiUrl } from "../../constants/apiPaths/apiPaths";
+import { useAppDispatch } from "../../store";
+import {
+  hideLoadingActionCreator,
+  showLoadingActionCreator,
+} from "../../store/ui/uiSlice";
 
 axios.defaults.baseURL = apiUrl;
 
 const useBugs = () => {
+  const dispatch = useAppDispatch();
+
   const getBugs = useCallback(async (): Promise<Bugs> => {
+    dispatch(showLoadingActionCreator());
+
     const { data: bugs } = await axios.get(apiPartialPaths.base);
 
+    dispatch(hideLoadingActionCreator());
+
     return bugs;
-  }, []);
+  }, [dispatch]);
 
   return {
     getBugs,
