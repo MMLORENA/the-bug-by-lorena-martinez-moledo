a30288db08cc396618445fc63a7bd1cb1d2f43fa
Lorena Martinez <lmartmol@gmail.com>
2023-09-17 13:33:50 +0200

    Add BugsTable with test

---
+++ src/components/BugsTable/BugsTable.tsx
@@ -0,0 +1,40 @@
+import { useAppSelector } from "../../store";
+import "./BugsTable.scss";
+
+const BugsTable = (): React.ReactElement => {
+  const { bugs } = useAppSelector((bugsState) => bugsState.bugsStore);
+
+  return (
+    <table className="table">
+      <thead>
+        <tr>
+          <th className="table__heading">Picture</th>
+          <th className="table__heading">Name</th>
+          <th className="table__heading">Category</th>
+          <th className="table__heading">Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+        {bugs.map((bug) => (
+          <tr className="bug" key={bug.id}>
+            <td>
+              <img
+                src={bug.picture}
+                alt={`${bug.name} avatar`}
+                className="bug__image"
+              />
+            </td>
+            <td className="bug__name">{bug.name}</td>
+            <td className="bug__category">{bug.category}</td>
+            <td className="bug__actions">
+              <button>Modify</button>
+              <button>Delete</button>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+export default BugsTable;
