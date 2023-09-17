74fc569fd53f9b14e3f3917d9638b85a7c1cb78f
Lorena Martinez <lmartmol@gmail.com>
2023-09-17 10:08:45 +0200

    Add store provider to app routing test

--- src/components/App/App.routing.test.tsx
+++ src/components/App/App.routing.test.tsx
@@ -1,13 +1,19 @@
 import { render, screen } from "@testing-library/react";
 import { RouterProvider } from "react-router-dom";
 import { appRouter } from "../../routers/routers";
+import { Provider } from "react-redux";
+import { store } from "../../store";
 
 describe("Given a root '/' path", () => {
   describe("When it's render", () => {
     test("Then it should 'The Bug' text inside", () => {
       const expectedText = "The Bug";
 
-      render(<RouterProvider router={appRouter} />);
+      render(
+        <Provider store={store}>
+          <RouterProvider router={appRouter} />
+        </Provider>
+      );
 
       const text = screen.getByText(expectedText);
 
