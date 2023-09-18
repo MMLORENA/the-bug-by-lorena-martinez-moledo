d22be4a7f7572a32e348c1b7b04b65d485a45983
Lorena Martinez <lmartmol@gmail.com>
2023-09-18 19:08:00 +0200

    Add browser router to CreateBugPage

--- src/pages/CreateBugPage/CreateBugPage.test.tsx
+++ src/pages/CreateBugPage/CreateBugPage.test.tsx
@@ -1,13 +1,17 @@
 import { screen } from "@testing-library/react";
 import renderWithProviders from "../../testUtils/renderWithProviders";
 import CreateBugPage from "./CreateBugPage";
+import { getBrowserRouter } from "../../routers/routers";
+import { RouterProvider } from "react-router-dom";
 
 describe("Given a CreateBugPage", () => {
+  const createBugPageRouter = getBrowserRouter(<CreateBugPage />);
+
   describe("When it's render", () => {
     test("Then it should show a 'Create a new bug' heading inside", () => {
       const headingText = /Create a new bug/i;
 
-      renderWithProviders(<CreateBugPage />);
+      renderWithProviders(<RouterProvider router={createBugPageRouter} />);
 
       const heading = screen.getByRole("heading", { name: headingText });
 
@@ -15,7 +19,7 @@ describe("Given a CreateBugPage", () => {
     });
 
     test("Then it should show a form", () => {
-      renderWithProviders(<CreateBugPage />);
+      renderWithProviders(<RouterProvider router={createBugPageRouter} />);
 
       const form = screen.getByRole("form");
 
