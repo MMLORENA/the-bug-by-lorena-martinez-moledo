3e86c43b30a92d7bbbfc492f5923c6630e89a872
Lorena Martinez <lmartmol@gmail.com>
2023-09-18 13:39:51 +0200

    Add create path to CreateBugPage

--- src/constants/routerPaths/routerPaths.ts
+++ src/constants/routerPaths/routerPaths.ts
@@ -1,8 +1,9 @@
-type routerPaths = "root" | "home";
+type routerPaths = "root" | "home" | "create";
 
 const routerPaths: Record<routerPaths, string> = {
   root: "/",
   home: "/home",
+  create: "/create",
 };
 
 export default routerPaths;
--- src/routers/routes.tsx
+++ src/routers/routes.tsx
@@ -2,6 +2,7 @@ import { Navigate, RouteObject } from "react-router-dom";
 import routerPaths from "../constants/routerPaths/routerPaths";
 import App from "../components/App/App";
 import BugsPage from "../pages/BugsPage/BugsPage";
+import CreateBugPage from "../pages/CreateBugPage/CreateBugPage";
 
 const routes: RouteObject[] = [
   {
@@ -10,6 +11,7 @@ const routes: RouteObject[] = [
     children: [
       { index: true, element: <Navigate to={routerPaths.home} replace /> },
       { path: routerPaths.home, element: <BugsPage /> },
+      { path: routerPaths.create, element: <CreateBugPage /> },
     ],
   },
 ];
