34f08fcf3d1cd3d0c9c9dfafd9b93f3ed4d5650d
Lorena Martinez <lmartmol@gmail.com>
2023-09-18 19:07:13 +0200

    Add handleOnSubmit function on CreateBugPage

--- src/constants/feedbackMessages/feedbackMessages.ts
+++ src/constants/feedbackMessages/feedbackMessages.ts
@@ -4,6 +4,10 @@ const feedbackMessages = {
     error: "Ups, error on delete bug",
     success: "Bug successfully deleted",
   },
+  createBug: {
+    error: "Ups, error creating a new bug",
+    success: "Bug successfully created",
+  },
 };
 
 export default feedbackMessages;
--- src/pages/CreateBugPage/CreateBugPage.tsx
+++ src/pages/CreateBugPage/CreateBugPage.tsx
@@ -1,10 +1,35 @@
+import { useNavigate } from "react-router-dom";
 import BugForm from "../../components/BugForm/BugForm";
+import feedbackMessages from "../../constants/feedbackMessages/feedbackMessages";
+import useBugs from "../../hooks/useBugs/useBugs";
+import { useAppDispatch } from "../../store";
+import { createBugActionCreator } from "../../store/bugs/bugsSlice";
+import { BugData } from "../../types";
+import showFeedback from "../../utils/showFeedback";
+import routerPaths from "../../constants/routerPaths/routerPaths";
 
 const CreateBugPage = (): React.ReactElement => {
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+  const { createBug } = useBugs();
+
+  const handleOnSubmit = async (bugData: BugData) => {
+    try {
+      const newBug = await createBug(bugData);
+
+      dispatch(createBugActionCreator(newBug));
+      showFeedback(feedbackMessages.createBug.success, "success");
+
+      navigate(routerPaths.root);
+    } catch {
+      showFeedback(feedbackMessages.createBug.error, "error");
+    }
+  };
+
   return (
     <>
       <h1 className="page-title">Create a new bug</h1>
-      <BugForm />
+      <BugForm onSubmit={handleOnSubmit} />
     </>
   );
 };
