d021f4ba304ebc99966f3857f8e55571eec64b15
Lorena Martinez <lmartmol@gmail.com>
2023-09-18 02:49:22 +0200

    Add test unsuccessfully deleted in BugsTest

--- src/components/BugsTable/BugsTable.test.tsx
+++ src/components/BugsTable/BugsTable.test.tsx
@@ -5,6 +5,9 @@ import { PreloadedState } from "@reduxjs/toolkit";
 import { RootState } from "../../store";
 import { getMockBug } from "../../factories/bugsFactory";
 import { bugzillaName } from "../../mocks/bugs/bugs";
+import userEvent from "@testing-library/user-event";
+import server from "../../mocks/server";
+import { errorsHandlers } from "../../mocks/handlers";
 
 describe("Given a BugsTable component", () => {
   describe("When it render", () => {
@@ -24,7 +27,7 @@ describe("Given a BugsTable component", () => {
   });
 
   describe("When it receives 'Bugzilla' bug data", () => {
-    const mockBugzilla = getMockBug({ name: bugzillaName });
+    const mockBugzilla = getMockBug({ name: bugzillaName, id: "1" });
 
     const mockPreloadedBugState: Partial<PreloadedState<RootState>> = {
       bugsStore: {
@@ -38,6 +41,38 @@ describe("Given a BugsTable component", () => {
       const bugzilla = screen.getByText(mockBugzilla.name);
 
       expect(bugzilla).toBeInTheDocument();
+    });
+
+    describe("And the user clicks on the 'delete' button of the bug", () => {
+      const buttonText = "Delete";
+
+      test("Then it shouldn't show 'Bugzilla' text", async () => {
+        renderWithProviders(<BugsTable />, mockPreloadedBugState);
+
+        const button = screen.getByRole("button", { name: buttonText });
+
+        await userEvent.click(button);
+
+        const bugzilla = screen.queryByText(bugzillaName);
+
+        expect(bugzilla).not.toBeInTheDocument();
+      });
+    });
+
+    describe("And the user clicks on the 'delete' button of the bug there is an error", () => {
+      test("Then it should show 'Bugzilla' text", async () => {
+        server.use(...errorsHandlers);
+
+        renderWithProviders(<BugsTable />, mockPreloadedBugState);
+
+        const button = screen.getByRole("button", { name: "Delete" });
+
+        await userEvent.click(button);
+
+        const bugzilla = screen.getByText(bugzillaName);
+
+        expect(bugzilla).toBeInTheDocument();
+      });
     });
   });
 });
